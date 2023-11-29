3b3c965a0173112668e07315c42788553524c82c
JosephPerez831 <josephperez4168@gmail.com>
2023-11-29 11:56:11 -0800

    added camera

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ from PIL import Image, ImageFile
 
 from keras.preprocessing.image import ImageDataGenerator
 
-train_dir = 'Training'
-val_dir = 'Testing'
+train_dir = '/Users/josep/Documents/emocrec/Training'
+val_dir = '/Users/josep/Documents/emocrec/Testing'
 
 batch_size = 64
 num_epoch = 50
@@ -75,76 +75,15 @@ def build_model(input_shape):
 
     return model
 
-# def build_model(input_shape):
-    # # input_layer = Input(shape=input_shape)
-    # # x = Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48,48,1))(input_layer)
-    # # x = Conv2D(64, kernel_size=(3, 3), activation='relu')(x)
-    # # x = MaxPooling2D(pool_size=(2, 2))(x)
-    # # x = Dropout(0.25)(x)
-    
-    # # x = Conv2D(128, kernel_size=(3, 3), activation='relu')(x)
-    # # x = MaxPooling2D(pool_size=(2, 2))(x)
-    # # x = Conv2D(128, kernel_size=(3, 3), activation='relu')(x)
-    # # x = MaxPooling2D(pool_size=(2, 2))(x)
-    # # x = Dropout(0.25)(x)
-    
-    # # x = Flatten()(x)
-    # # x = Dense(1024, activation='relu')(x)
-    # # x = Dropout(0.5)(x)
-    # # x = Dense(6, activation='softmax')(x)
-    
-    # input_layer = Input(shape=input_shape)
-    # # x = Conv2D(16, (3, 3), padding='same')(input_layer)
-    # # x = Activation('relu')(x)
-    # # x = MaxPooling2D(pool_size=(2, 2))(x)
-    
-    # # x = Conv2D(32, (3, 3), padding='same')(input_layer)
-    # # x = Activation('relu')(x)
-    # # x = MaxPooling2D(pool_size=(2, 2))(x)
-    
-    # # x = Conv2D(64, (3, 3), padding='same')(x)
-    # # x = Activation('relu')(x)
-    # # x = MaxPooling2D(pool_size=(2, 2))(x)
-
-    # # x = Conv2D(128, (3, 3), padding='same')(x)
-    # # x = Activation('relu')(x)
-    # # x = MaxPooling2D(pool_size=(2, 2))(x)
-
-    # # x = GlobalAveragePooling2D()(x)
-
-    # # x = Flatten()(x)
-    # # x = Dense(512)(x)
-    # # x = Activation('relu')(x)
-    # # x = Dropout(0.5)(x)
-
-    # # # # x = Dense(1, activation='sigmoid')(x)  # Binary classification
-    # # # # x = Dense(2, activation='softmax')(x)
-    # # x = Dense(6, activation='softmax')(x)
-
-
-    # model = Model(inputs=input_layer, outputs=x)
-    # return model
 
 input_shape = (48,48, 1)
 model = build_model(input_shape)
-# # model.compile(optimizer=Adam(), loss='binary_crossentropy', metrics=['accuracy'])
 
-# # model.compile(optimizer=Adam(), loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.compile(loss='categorical_crossentropy',optimizer=Adam(),metrics=['accuracy'])
 reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2,
                               patience=5, min_lr=0.001)
-# model_info = model.fit(
-#         train_generator,
-#         batch_size=batch_size,
-#         epochs=num_epoch,
-#         # validation_data=validation_generator,
-#         callbacks=[reduce_lr])
-# model.load_weights('model1.h5')
 
-# model.evaluate(validation_generator, batch_size=batch_size)
-
-# model.save_weights('model1.h5')
 
 model.load_weights('model1.h5')
 
@@ -160,7 +99,7 @@ img = None
 
 option = st.selectbox(
    "Choose a method for uploading images:",
-   ("From URL", "From Computer"),
+   ("From URL", "From Computer", "From Camera"),
    index=None,
    placeholder="Select a method...",
 )
@@ -180,7 +119,32 @@ if(option=="From URL"):
         o_img = Image.open(url_response)
         o_img = np.array(o_img)
         img = copy(o_img)
-    
+if(option=="From Camera"):
+    cam = cv2.VideoCapture(0)
+    cv2.namedWindow("test")
+    img_counter = 0
+    o_img = None
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("failed to grab frame")
+            break
+        cv2.imshow("test", frame)
+        k = cv2.waitKey(1)
+        if k%256 == 32:
+            # SPACE pressed
+            img_name = "opencv_frame_{}.png".format(img_counter)
+            o_img = img_name
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            break
+    o_img = Image.open(o_img)
+    o_img = np.array(o_img)
+    img = o_img
+
+    cam.release()
+    cv2.destroyAllWindows()
+     
 if(option=="From Computer"):
     uploaded_img = st.file_uploader('Choose an image', type=['jpg', 'png'])
     if uploaded_img:
@@ -188,13 +152,9 @@ if(option=="From Computer"):
         o_img = np.array(o_img)
         img = copy(o_img)
 
-    
 if img is not None:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    faces = face_classifier.detectMultiScale(
-        gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40)
-    )
+    faces = face_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(40, 40))
 
     for (x, y, w, h) in faces:
         cv2.rectangle(o_img, (x, y), (x + w, y + h), (0, 255, 0), 4)
@@ -205,18 +165,3 @@ if img is not None:
         cv2.putText(o_img, emotion_dict[maxindex], (x, y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
     st.image(o_img)
-
-
-# Using cv2.imshow() method 
-# Displaying the image 
-# cv2.imshow("predicted result", img) 
-
-# # waits for user to press any key 
-# # (this is necessary to avoid Python kernel form crashing) 
-# cv2.waitKey(0) 
-  
-# # closing all open windows 
-# cv2.destroyAllWindows() 
-
-# cv2.imwrite("output.png", img) 
-
